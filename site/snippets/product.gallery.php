a5a0a6b9ff35e484427d4d8271adb29124700979
Sam Nabi <sam@samnabi.com>
2015-09-13 12:49:21 +0100

    New logo, new gallery layout

--- site/snippets/product.gallery.php
+++ site/snippets/product.gallery.php
@@ -1,26 +1,28 @@
 <?php if ($page->hasImages()) { ?>
 	<section class="gallery">
+		
 		<?php $first = true ?>
 		<?php foreach ($page->images() as $photo) { ?>	
-
 			<!-- Radio button (hidden with CSS) -->
-			<?php if ($first) { ?>
-				<?php $first = false ?>
-				<input checked type="radio" name="thumbnail" id="<?php echo $photo->hash() ?>">
-	   		<?php } else { ?>
-				<input type="radio" name="thumbnail" id="<?php echo $photo->hash() ?>">
-			<?php } ?>
+			<input <?php ecco($first,'checked') ?> type="radio" name="thumbnail" id="<?php echo $photo->hash() ?>">
+			<?php $first = false ?>
 			
-			<!-- Full-size photo -->
-			<img src="<?php echo thumb($photo,array('height'=>300, 'quality'=>90))->dataUri() ?>" title="<?php echo $photo->title() ?>"/>
-
-			<!-- Show thumbnails if there are multiple photos -->
-			<?php if (count($page->images()) > 1) { ?>
-				<label for="<?php echo $photo->hash() ?>">
-					<img src="<?php echo thumb($photo,array('width'=>100,'height'=>100, 'quality'=>90, 'crop'=>true))->dataUri() ?>" title="<?php echo $photo->title() ?>"/>
-				</label>
-			<?php } ?>
-			
+			<!-- Large photo -->
+			<img src="<?php echo thumb($photo,array('height'=>300))->dataUri() ?>" title="<?php echo $photo->title() ?>"/>
 		<?php } ?>
+
+		<!-- Show thumbnails if there are multiple photos -->
+		<?php if ($page->images()->count() > 1) { ?>
+			<ul class="small-block-grid-3 large-block-grid-4">
+				<?php foreach ($page->images() as $photo) { ?>	
+					<li>
+						<label for="<?php echo $photo->hash() ?>">
+							<img src="<?php echo thumb($photo,array('width'=>100,'height'=>100, 'quality'=>80, 'crop'=>true))->dataUri() ?>" title="<?php echo $photo->title() ?>"/>
+						</label>
+					</li>
+				<?php } ?>
+			</ul>
+		<?php } ?>
+
 	</section>
 <?php } ?>
