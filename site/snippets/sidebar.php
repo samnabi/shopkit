115c04448b6249e70cd5d9bf395faab8b3b27e5a
Sam Nabi <sam@samnabi.com>
2015-09-27 20:46:01 -0400

    Improve cart item styles; add icons to user navbar

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="uk-width-small-1-1 uk-width-medium-1-3 uk-pull-2-3">
+<aside class="sidebar uk-width-small-1-1 uk-width-medium-1-3 uk-pull-2-3">
 
     <!-- Logo -->
     <div class="uk-panel uk-panel-divider uk-margin-top uk-hidden-small uk-text-center">
