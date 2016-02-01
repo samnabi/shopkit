080f4e6cd655d80dad944cf03caf508f008540b7
Sam Nabi <sam@samnabi.com>
2016-02-01 13:46:22 -0500

    Shrink large image uploads

--- site/config/config.php
+++ site/config/config.php
@@ -270,3 +270,27 @@ function makeVisible($page) {
     return response::error($e->getMessage());
   }
 }
+
+// Shrink large images on upload
+kirby()->hook('panel.file.upload', 'shrinkImage');
+kirby()->hook('panel.file.replace', 'shrinkImage');
+function shrinkImage($file, $maxDimension = 1000) {
+  try {
+    if ($file->type() == 'image' and ($file->width() > $maxDimension or $file->height() > $maxDimension)) {
+      
+      // Get original file path
+      $originalPath = $file->dir().'/'.$file->filename();
+
+      // Create a thumb and get its path
+      $resized = $file->resize($maxDimension,$maxDimension);
+      $resizedPath = $resized->dir().'/'.$resized->filename();
+
+      // Replace the original file with the resized one
+      copy($resizedPath, $originalPath);
+      unlink($resizedPath);
+
+    }
+  } catch(Exception $e) {
+    return response::error($e->getMessage());
+  }
+}
