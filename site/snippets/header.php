5bfd60d4a98d248fc800ea23bdbc67fc8e464ecb
Sam Nabi <sam@samnabi.com>
2016-03-03 10:32:31 -0500

    Add naksh font for arabic

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,11 +3,13 @@
 <head>
 
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 
+	<!-- Styles -->
 	<?php echo css(getStylesheet($site->colorbase(),$site->coloraccent(),$site->colorlink())) ?>
+	<?php echo css('assets/css/font.naksh.css') ?>
 	<?php echo css('assets/css/font.asap.css') ?>
 	<?php snippet('header.background.style') ?>
 
