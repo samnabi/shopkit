bd97801ede2a7f219f41500649ab54d9ef4d80f0
Sam Nabi <sam@samnabi.com>
2016-07-26 20:37:33 -0400

    Remove seletor from registry

--- site/plugins/shopkit/registry/fields.php
+++ site/plugins/shopkit/registry/fields.php
@@ -1,8 +1,7 @@
 <?php
 
 // Register fields
-$kirby->set('field', 'color',      __DIR__.DS.'..'.DS.'fields'.DS.'color');
-$kirby->set('field', 'markdown',      __DIR__.DS.'..'.DS.'fields'.DS.'markdown');
-$kirby->set('field', 'place',      __DIR__.DS.'..'.DS.'fields'.DS.'place');
-$kirby->set('field', 'selector',      __DIR__.DS.'..'.DS.'fields'.DS.'selector');
-$kirby->set('field', 'tabs',      __DIR__.DS.'..'.DS.'fields'.DS.'tabs');
+$kirby->set('field', 'color',    __DIR__.DS.'..'.DS.'fields'.DS.'color');
+$kirby->set('field', 'markdown', __DIR__.DS.'..'.DS.'fields'.DS.'markdown');
+$kirby->set('field', 'place',    __DIR__.DS.'..'.DS.'fields'.DS.'place');
+$kirby->set('field', 'tabs',     __DIR__.DS.'..'.DS.'fields'.DS.'tabs');
