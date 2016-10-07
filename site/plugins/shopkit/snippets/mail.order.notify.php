9738b7f88242aa1e99f469b37195f28ef4f5f762
Sam Nabi <sam@samnabi.com>
2016-10-07 13:34:04 -0400

    Fix notification email issue (Closes #93)

--- site/plugins/shopkit/snippets/mail.order.notify.php
+++ site/plugins/shopkit/snippets/mail.order.notify.php
@@ -1,5 +1,10 @@
 <?php
 
+// Set detected language
+site()->visit('shop', (string) site()->detectedLanguage());
+site()->kirby->localize();
+
+// Build body text
 $body = l::get('order-notification-message').' ';
 $body .= page('shop/orders')->url().'?txn_id='.$txn->txn_id()."\n\n";
 $body .= l::get('transaction-id').' '.$txn->txn_id()."\n\n";
@@ -13,12 +18,15 @@ foreach ($txn->products()->toStructure() as $item) {
   $body .= "\n".'Qty: '.$item->quantity()."\n\n";
 }
 
+// Build the email
 $email = new Email(array(
   'to'      => $n->email(),
   'from'    => 'noreply@'.server::get('server_name'),
   'subject' => l::get('order-notification-subject'),
   'body'    => $body,
 ));
+
+// Send it
 $email->send();
 
 ?>
--- site/plugins/shopkit/snippets/mail.order.tamper.php
+++ site/plugins/shopkit/snippets/mail.order.tamper.php
@@ -1,15 +1,23 @@
 <?php
-	// Notify the site's PayPal email address
-    
-    $body = l::get('transaction-id').' '.$txn->txn_id()."\n\n";
-    $body .= l::get('order-error-message-tamper').' ';
-    $body .= page('shop/orders')->url().'?txn_id='.$txn->txn_id();
 
-    $email = new Email(array(
-      'to'      => page('shop')->error_email()->value,
-      'from'    => 'noreply@'.server::get('server_name'),
-      'subject' => l::get('order-error-subject'),
-      'body'    => $body,
-    ));
-    $email->send();
+// Set detected language
+site()->visit('shop', (string) site()->detectedLanguage());
+site()->kirby->localize();
+    
+// Build body text
+$body = l::get('transaction-id').' '.$txn->txn_id()."\n\n";
+$body .= l::get('order-error-message-tamper').' ';
+$body .= page('shop/orders')->url().'?txn_id='.$txn->txn_id();
+
+// Build the email
+$email = new Email(array(
+  'to'      => page('shop')->error_email()->value,
+  'from'    => 'noreply@'.server::get('server_name'),
+  'subject' => l::get('order-error-subject'),
+  'body'    => $body,
+));
+
+// Send it
+$email->send();
+
 ?>
