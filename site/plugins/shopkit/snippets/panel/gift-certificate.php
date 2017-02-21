3fc0d7073c26e4325a8b6d9ef959c2a480863e70
Sam Nabi <sam@samnabi.com>
2017-02-21 16:56:49 -0500

    Add preview link to gift certificate snippet

--- site/plugins/shopkit/snippets/panel/gift-certificate.php
+++ site/plugins/shopkit/snippets/panel/gift-certificate.php
@@ -1,2 +1,5 @@
 <span class="badge"><?= formatPrice($values->amount()) ?> left</span>
+
 <?= $values->code() ?>
+
+<br><small><input type="text" value="<?= site()->url() ?>/gift/<?= $values->code() ?>" readonly></small>
