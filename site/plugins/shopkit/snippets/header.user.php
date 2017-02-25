b696da5620ea2434259a9bd7824ecbceadd7285b
Sam Nabi <sam@samnabi.com>
2017-02-24 19:22:40 -0500

    Redesign login form

--- site/plugins/shopkit/snippets/header.user.php
+++ site/plugins/shopkit/snippets/header.user.php
@@ -51,27 +51,23 @@
         
         <form dir="auto" action="<?= url('/login') ?>" method="POST" id="login" class="uk-form">
             <input type="hidden" name="redirect" value="<?= $page->uri() ?>">
-            <div class="uk-grid uk-grid-width-1-2">
-                <div>
-                  <label class="uk-width-1-1" for="email"><?= l::get('email-address') ?></label>
-                  <input class="uk-width-1-1" type="text" id="email" name="email">
+            <div class="uk-grid uk-grid-small">
+                <div class="uk-width-2-6 uk-width-large-1-4">
+                  <label class="uk-text-small uk-width-1-1" for="email"><?= l::get('email-address') ?></label>
+                  <input class="uk-form-small uk-width-1-1" type="text" id="email" name="email">
                 </div>
-                <div>
-                  <label class="uk-width-1-1" for="password"><?= l::get('password') ?></label>
-                  <input class="uk-width-1-1" type="password" id="password" name="password">
+                <div class="uk-width-2-6 uk-width-large-1-4">
+                  <label class="uk-text-small uk-width-1-1" for="password"><?= l::get('password') ?></label>
+                  <input class="uk-form-small uk-width-1-1" type="password" id="password" name="password">
                 </div>
-            </div>
-            <div class="uk-margin">
-                <button class="uk-button uk-width-1-1" type="submit" name="login">
+                <div class="uk-width-2-6">
+                    <label>&nbsp;</label>
+                  <button class="uk-button uk-button-small uk-width-1-1" type="submit" name="login">
                     <?= l::get('login') ?>
-                </button>
-            </div>
-            <div class="uk-text-small uk-grid uk-grid-width-1-2">
-                <div>
-                    <a href="<?= url('account/reset') ?>" title="<?= l::get('forgot-password') ?>"><?= l::get('forgot-password') ?></a>
+                  </button>
                 </div>
-                <div class="uk-text-right">
-                    <?= l::get('new-customer') ?>
+                <div class="uk-text-small uk-width-1-1">
+                    <a href="<?= url('account/reset') ?>" title="<?= l::get('forgot-password') ?>"><?= l::get('forgot-password') ?></a>&nbsp;&nbsp;&nbsp;
                     <a href="<?= url('account/register') ?>" title="<?= l::get('register') ?>"><?= l::get('register') ?></a>
                 </div>
             </div>
