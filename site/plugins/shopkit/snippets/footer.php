011a917a4f0a357f5447fab365ccea396fdf704a
Sam Nabi <sam@samnabi.com>
2017-03-15 18:19:08 -0400

    Slideshow design, progressive font loading, hide toggle animations

--- site/plugins/shopkit/snippets/footer.php
+++ site/plugins/shopkit/snippets/footer.php
@@ -1,14 +1,7 @@
 </div><!-- .wrapper -->
 
 <?= js('assets/plugins/shopkit/js/expand-collapse.min.js'); ?>
-
-<?= js('assets/plugins/shopkit/js/fontfaceobserver.standalone.js'); ?>
-<script>
-  // Load font progressively (https://www.filamentgroup.com/lab/font-events.html)
-  new FontFaceObserver('asap').load().then(function() {
-    document.documentElement.className += " fonts-loaded";
-  });
-</script>
+<?= js('assets/plugins/shopkit/js/load-fonts.js'); ?>
 
 </body>
 </html>
--- site/plugins/shopkit/snippets/slideshow.product.php
+++ site/plugins/shopkit/snippets/slideshow.product.php
@@ -2,17 +2,17 @@
 
 <nav class="slideshow-nav">
 
-	<a id="slideshow-prev" <?php if($product->hasPrevVisible()){ echo 'href="'.$product->prevVisible()->url().'/slide"'; } else { echo 'disabled'; } ?> >
+	<a class="button" id="slideshow-prev" <?php if($product->hasPrevVisible()){ echo 'href="'.$product->prevVisible()->url().'/slide"'; } else { echo 'disabled'; } ?> >
 		<?= f::read('site/plugins/shopkit/assets/svg/arrow-left.svg') ?>
 		<span><?= l('prev') ?></span>
 	</a>
 
-    <a id="view-grid" href="<?= $product->parent()->url() ?>">
-    	<?= f::read('site/plugins/shopkit/assets/svg/grid.svg') ?>
-    	<span><?= l('view-grid') ?></span>
-    </a>
+  <a class="button" id="view-grid" href="<?= $product->parent()->url() ?>">
+  	<?= f::read('site/plugins/shopkit/assets/svg/grid.svg') ?>
+  	<span><?= l('view-grid') ?></span>
+  </a>
 
-	<a id="slideshow-next" <?php if($product->hasNextVisible()){ echo 'href="'.$product->nextVisible()->url().'/slide"'; } else { echo 'disabled'; } ?> >
+	<a class="button" id="slideshow-next" <?php if($product->hasNextVisible()){ echo 'href="'.$product->nextVisible()->url().'/slide"'; } else { echo 'disabled'; } ?> >
 		<?= f::read('site/plugins/shopkit/assets/svg/arrow-right.svg') ?>
 		<span><?= l('next') ?></span>
 	</a>
@@ -20,47 +20,45 @@
 
 <a class="slideshow" href="<?= $product->url() ?>" vocab="http://schema.org" typeof="Product">
   <?php if ($product->hasImages()) { ?>
-	  <div class="image">
-      <?php
-  		  $image = $product->images()->sortBy('sort', 'asc')->first();
-	 		  $thumb = $image->thumb(['width' => 600]);
-		  ?>
-		  <img property="image" content="<?= $thumb->url() ?>" src="<?= $thumb->url() ?>" title="<?= $product->title() ?>">
-	  </div>
+    <?php
+  		$image = $product->images()->sortBy('sort', 'asc')->first();
+	 		$thumb = $image->thumb(['width' => 600]);
+		?>
+		<img property="image" content="<?= $thumb->url() ?>" src="<?= $thumb->url() ?>" title="<?= $product->title() ?>">
   <?php } ?>
 
 	<div class="description">
+    <?php if ($product->brand()->isNotEmpty()) { ?>
+      <p class="brand"><?= $product->brand() ?></p>
+    <?php } ?>
 		<h3 dir="auto" property="name"><?= $product->title()->html() ?></h3>
 		
-		<?php if ($product->text() != '') { ?>
-			<p dir="auto" property="description"><?= $product->text()->excerpt(80) ?></p>
-		<?php } ?>
-		
-		<div dir="auto">
-    		<?php
-    			$variants = $product->variants()->yaml();
+		<p class="price" property="offers" typeof="Offer">
+  		<?php
+  			$variants = $product->variants()->yaml();
 
-    			foreach ($variants as $variant) {
-    				$pricelist[] = $variant['price'];
-    				$salepricelist[] = salePrice($variant);
-    			}
+  			foreach ($variants as $variant) {
+  				$pricelist[] = $variant['price'];
+  				$salepricelist[] = salePrice($variant);
+  			}
 
-    			$priceFormatted = is_array($pricelist) ? formatPrice(min($pricelist)) : 0;
-    			if (count($variants) > 1) $priceFormatted = l('from').' '.$priceFormatted;
+  			$priceFormatted = is_array($pricelist) ? formatPrice(min($pricelist)) : 0;
+  			if (count($variants) > 1) $priceFormatted = l('from').' '.$priceFormatted;
 
-    			$saleprice = $salepricelist[min(array_keys($pricelist, min($pricelist)))];
+  			$saleprice = $salepricelist[min(array_keys($pricelist, min($pricelist)))];
+
+				if ($saleprice) {
+					echo formatPrice($saleprice);
+					echo '<del>'.$priceFormatted.'</del>';
+				} else {
+					echo $priceFormatted;
+				}
 			?>
-			<span property="offers" typeof="Offer">
-				<?php
-					if ($saleprice) {
-						echo formatPrice($saleprice);
-						echo '<del>'.$priceFormatted.'</del>';
-					} else {
-						echo $priceFormatted;
-					}
-				?>
-			</span>
-		</div>
+		</p>
+
+    <?php if ($product->text()->isNotEmpty()) { ?>
+      <p dir="auto" property="description"><?= $product->text()->excerpt(300) ?></p>
+    <?php } ?>
 	</div>
 
 </a>
