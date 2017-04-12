a4a14d98fa0cfeebc1c3336b7936730255d8e36c
Sam Nabi <sam@samnabi.com>
2017-04-12 09:20:14 -0400

    Update order.create to use file-based transaction logging

--- site/plugins/shopkit/snippets/order.create.php
+++ site/plugins/shopkit/snippets/order.create.php
@@ -3,10 +3,8 @@
 if(r::is('post') and get('subject') != '') go(url('error'));
 
 $site = site();
-
-$cart = Cart::getCart();
-
-$discount = getDiscount($cart);
+$user = $site->user();
+$timestamp = date('U');
 
 // Set transaction status
 if (get('giftCertificatePaid') == 'true') {
@@ -15,77 +13,67 @@ if (get('giftCertificatePaid') == 'true') {
 	$status = 'pending';
 }
 
-// Set the timestamp so txn-id and txn-date use the same value
-$timestamp = date('U');
-
-// Create a YAML-structured list of products
-$items = [];
-foreach ($cart->getItems() as $i => $item) {
-	// Look for downloads
-	foreach (page($item->uri)->variants()->toStructure() as $variant) {
-		if (str::slug($variant->name()) == $item->variant) {
+// Add download links
+foreach (getItems() as $i => $item) {
+	foreach (page($item->uri())->variants()->toStructure() as $variant) {
+		if (str::slug($variant->name()) == $item->variant()) {
 			if ($variant->download_files()->isNotEmpty()) {
 
 				// Build full URLs for downloads
 				$files = [];
 				foreach (explode(',', $variant->download_files()) as $filename) {
-					$files[] = url($item->uri).'/'.$filename;
+					$files[] = url($item->uri()).'/'.$filename;
 				}
-
-				// Add downloads properties
-				$item->downloads = [
+				$downloads = [
 					'files' => $files,
 					'expires' => $variant->download_days()->isEmpty() ? NULL : $timestamp + ($variant->download_days()->value * 60 * 60 * 24)
 				];
+
+				// Update transaction file
+				page(s::get('txn'))->update([
+					'downloads' => yaml::encode($downloads)
+				]);
 			}
 		}
 	} 
-	$items[] = (array)$item;
 }
 
-// Unique transaction ID
-$txn_id = get('gateway').'-'.$timestamp.'-'.bin2hex(openssl_random_pseudo_bytes(2));
+// Add transaction details
+$discount = getDiscount();
+page(s::get('txn'))->update([
+	'txn-date'  => $timestamp,
+	'txn-currency' => $site->currency_code(),
+	'status'  => $status,
+	'subtotal' => number_format(cartSubtotal(getItems()),2,'.',''),
+	'discount' => number_format($discount['amount'],2,'.',''),
+	'tax' => number_format(cartTax(),2,'.',''),
+	'giftcertificate' => null !== get('giftCertificateAmount') ? number_format(get('giftCertificateAmount'),2,'.','') : '0.00',
+]);
 
-try {
-	// Create the pending order file
-	page('shop/orders')->children()->create($txn_id, 'order', [
-		'txn-id' => $txn_id,
-		'txn-date'  => $timestamp,
-		'txn-currency' => $site->currency_code(),
-		'status'  => $status,
-		'products' => "\n".yaml::encode($items),
-		'subtotal' => number_format(cartSubtotal(getItems()),2,'.',''),
-		'discount' => number_format($discount['amount'],2,'.',''),
-		'tax' => number_format($cart->getTax(),2,'.',''),
-		'giftcertificate' => null !== get('giftCertificateAmount') ? number_format(get('giftCertificateAmount'),2,'.','') : '0.00',
-	]);
+// Add payer info if it's available at this point
+if ($user) {
+	page(s::get('txn'))->update([
+		'payer-id' => $user->username(),
+		'payer-name' => $user->firstname().' '.$user->lastname(),
+		'payer-email' => $user->email(),
+		'payer-address' => page('shop/countries/'.$user->country())->title()
+	], $site->defaultLanguage()->code());
+}	
 
-	// Add payer info if it's available at this point
-	if ($user = $site->user()) {
-		page('shop/orders/'.$txn_id)->update([
-			'payer-id' => $user->username(),
-			'payer-name' => $user->firstname().' '.$user->lastname(),
-			'payer-email' => $user->email(),
-			'payer-address' => page('shop/countries/'.$user->country())->title()
-		], $site->defaultLanguage()->code());
-	}	
-
-	// Update the giftcard balance
-	if ($giftCertificateRemaining = get('giftCertificateRemaining')) {
-		$certificates = $site->gift_certificates()->yaml();
-		foreach ($certificates as $key => $certificate) {
-			if (strtoupper($certificate['code']) == page('shop/orders/'.$txn_id)->giftcode()) {
-				$certificates[$key]['amount'] = number_format($giftCertificateRemaining,2,'.','');
-			}
+// Update the site's giftcard balance
+if ($giftCertificateRemaining = get('giftCertificateRemaining')) {
+	$certificates = $site->gift_certificates()->yaml();
+	foreach ($certificates as $key => $certificate) {
+		if (strtoupper($certificate['code']) == page(s::get('txn'))->giftcode()) {
+			$certificates[$key]['amount'] = number_format($giftCertificateRemaining,2,'.','');
 		}
-		$site->update(['gift-certificates' => yaml::encode($certificates)], $site->defaultLanguage()->code());
 	}
-} catch(Exception $e) {
-	// Order creation failed
-	echo $e->getMessage();
+	$site->update([
+		'gift-certificates' => yaml::encode($certificates)
+	], $site->defaultLanguage()->code());
 }
 
 // Redirect to self with GET, passing along the order ID
-go('shop/cart/process/'.get('gateway').'/'.$txn_id);
+go('shop/cart/process/'.get('gateway').'/'.page(s::get('txn'))->txn_id());
 
 ?>
