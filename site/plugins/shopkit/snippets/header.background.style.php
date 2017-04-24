cc435dcc1b64e98a71a0bd65a513e765be84242a
Sam Nabi <sam@samnabi.com>
2017-04-24 14:26:31 -0400

    Frosted glass background image effect

--- site/plugins/shopkit/snippets/header.background.style.php
+++ site/plugins/shopkit/snippets/header.background.style.php
@@ -1,15 +1,12 @@
 <?php
-  // Background image & colour
+  // Background colour
   if ($site->backgroundcolor() != '' and $site->backgroundcolor() != '#FFFFFF') {
   	echo '<style> body { background-color: '.$site->backgroundcolor().'; } </style>';
   }
 
+  // Background image
   if ($site->backgroundimage() != '') {
-    if ($site->backgroundblur()->bool()) {
-      $bg = $site->backgroundimage()->toFile()->thumb(['width' => 2000, 'quality' => 80, 'blur' => true, 'upscale' => false]);
-    } else {
-      $bg = $site->backgroundimage()->toFile()->thumb(['width' => 2000, 'quality' => 80, 'upscale' => false]);
-    }
-    echo '<style> body { background-image: url('.$bg->url().'); } </style>';
+    $bg = $site->backgroundimage()->toFile()->thumb(['width' => 2000, 'quality' => 80, 'upscale' => false]);
+    echo '<style> body, .wrapper::before { background-image: url('.$bg->url().'); } </style>';
   }
 ?>
