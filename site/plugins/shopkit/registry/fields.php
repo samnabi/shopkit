8ccb0abba92041b34f6176d18343a43ebff506b2
Sam Nabi <sam@samnabi.com>
2017-07-11 19:18:07 -0400

    Update submodules

--- site/plugins/shopkit/registry/fields.php
+++ site/plugins/shopkit/registry/fields.php
@@ -4,6 +4,5 @@
 c::set('snippetfield.path', kirby()->roots()->plugins().DS.'shopkit'.DS.'snippets'.DS.'panel');
 
 // Register fields
-$kirby->set('field', 'color',        __DIR__.DS.'..'.DS.'fields'.DS.'color');
 $kirby->set('field', 'tabs',         __DIR__.DS.'..'.DS.'fields'.DS.'tabs');
 $kirby->set('field', 'tiers',        __DIR__.DS.'..'.DS.'fields'.DS.'tiers');
